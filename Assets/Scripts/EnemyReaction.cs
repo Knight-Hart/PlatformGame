c6d6d7bcd46843ba04f299e2edc43582ba892caa
KnightHart <steveperson666@gmail.com>
2019-04-19 17:27:02 +0300

    Added Files
    Added all project files

---
+++ Assets/Scripts/EnemyReaction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class EnemyReaction : MonoBehaviour {
+ 
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.CompareTag ("Enemy")) {
+		SceneManager.LoadScene ("Scene1");
+		}
+	}
+}
